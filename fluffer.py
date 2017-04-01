c206f5b5206a2d4d398b32716b887accbf15f9fb
mwang370 <mwang370@gatech.edu>
2017-04-01 15:53:23 -0400

    added try except

--- fluffer.py
+++ fluffer.py
@@ -2,6 +2,9 @@ from fluffer_helper import *
 from random import randint
 
 while True:
-    links = getLinks(getSearchTerm(randint(1, 5)))
-    link = links[randint(0, len(links) - 1)]
-    visitURL(link)
+    try:
+        links = getLinks(getSearchTerm(randint(1, 4)))
+        link = links[randint(0, len(links) - 1)]
+        visitURL(link)
+    except:
+        pass
