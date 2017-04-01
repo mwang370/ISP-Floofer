dcab3f3fe6721352e105a784eb4f03765ebe7e33
mwang370 <mwang370@gatech.edu>
2017-04-01 15:39:55 -0400

    small bugfix

--- fluffer_helper.py
+++ fluffer_helper.py
@@ -7,10 +7,10 @@ def getRandomWord():
     r = requests.get('http://www.setgetgo.com/randomword/get.php')
     return r.text
 
-def getLinks(word):
+def getLinks(serachTerm):
     headers = {'Ocp-Apim-Subscription-Key': 'a43582571a4946d0a3e7afb34d09807d'}
-    params = {'q': word}
-    r = requests.get('https://api.cognitive.microsoft.com/bing/v5.0/search?q=' + word + '&count=10', headers=headers, params=params)
+    params = {'q': searchTerm}
+    r = requests.get('https://api.cognitive.microsoft.com/bing/v5.0/search?q=' + searchTerm + '&count=10', headers=headers, params=params)
     results = r.json()
     results = results['webPages']['value']
     links = []
