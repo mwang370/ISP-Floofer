8f4db37b4274bfeab126117a1937236c4764a938
mwang370 <mwang370@gatech.edu>
2017-04-02 12:01:14 -0400

    bug fix

--- fluffer.py
+++ fluffer.py
@@ -3,7 +3,7 @@ from random import randint
 
 while True:
     try:
-        searchTerm = getSearchTerm(randint(1, 4))
+        searchTerm = getSearchTerm(1)
         print 'searching: ' + searchTerm
         links = getLinks(searchTerm)
         link = links[randint(0, len(links) - 1)]
